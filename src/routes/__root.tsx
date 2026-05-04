1ebd300f82778452909c1cd17f110e600b6393d4
Selin A Bacaz <selinb@udel.edu>
2026-05-03 23:16:38 -0400

    trying to fix deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import {  Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import Footer from '../components/Footer'
-import Header from '../components/Header'
+
 
 import appCss from '../styles.css?url'
 
